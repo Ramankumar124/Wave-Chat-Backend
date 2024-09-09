0a72a55556126be4c66bcb7fe56a74b54e29b92b
raman kumar <241ramankumar@gmail.com>
2024-09-09 22:34:33 +0530

    some modification

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
-const mongoose=requrie('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/Wave Chat");
 
-const userSchema=mongoose.Schema
+const userSchema = mongoose.Schema({
+    name: String,
+    phoneNo: Number,
+    messages: [{
+        type: mongoose.Schema.Types.ObjectId
+    }
+    ]
+})
+module.exports=mongoose.model("user",userSchema);
