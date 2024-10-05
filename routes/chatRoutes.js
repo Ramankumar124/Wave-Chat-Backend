345a193d1730a06dc0eae9b1b8940f77609918bd
raman kumar <241ramankumar@gmail.com>
2024-10-06 00:38:57 +0530

    chore:creatind infite scroll

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -5,29 +5,58 @@ const chatModel = require("../models/chat");
 
 
 
-router.get('/:contactUserId',isLogin ,async(req, res) => {
-    try {
-      const userId = req.user._id;
-      const contactUserId = req.params.contactUserId;
-      
-      console.log(`Server user id: ${userId}, contact id: ${contactUserId}`);
-  
-      let chat = await chatModel.findOne({
-        participent: { $all: [userId, contactUserId] }
-      }).populate('messages');
-  
-      // console.log("Chats are: ", chat);
-  
-      if (!chat) {
-        console.log("No chat found");
-        return res.status(404).json({ message: 'No chat found.' });
-      }
-  
-      res.status(200).json(chat.messages);
-    } catch (error) {
-      console.log("Server Error: ", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
+router.get('/:contactUserId', isLogin, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const contactUserId = req.params.contactUserId;
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = 5;  // Limit the number of messages per page
+    
+    // Step 1: Count messages specific to the chat between two users
+    const chat = await chatModel.findOne({
+      participent: { $all: [userId, contactUserId] }
+    });
+
+    if (!chat) {
+      console.log("No chat found");
+      return res.status(404).json({ 
+        message: 'No chat found.' 
+      });
     }
-  });
+
+    const totalMessages = chat.messages.length;  // Count the total number of messages in the chat
+    const totalPages = Math.ceil(totalMessages / limit);
+    const nextPage = page < totalPages ? page + 1 : null;
+
+    let skip = (page - 1) * limit;
+
+    // Step 2: Fetch the messages with pagination
+    let paginatedChat = await chatModel.findOne({
+      participent: { $all: [userId, contactUserId] }
+    }).populate({
+      path: 'messages',
+      options: {
+        skip: skip,
+        limit: limit,
+        sort: { createdAt: -1 }, 
+      }
+    });
+
+    res.status(200).json({
+      success: true,
+      msg: "User Messages",
+      data: paginatedChat.messages,
+      page,
+      nextPage,
+      totalPages,
+      totalMessages
+    });
+
+  } catch (error) {
+    console.log("Server Error: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
   module.exports=router;
